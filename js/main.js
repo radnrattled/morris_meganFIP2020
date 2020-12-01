0dab466e6835e5418a9bc2153eacccca658ca39e
Megan Morris <megs@Mac-mini.local>
2020-11-30 20:27:07 -0500

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { SendMail } from "./components/mailer.js";
+import { fetchData, postData } from "./modules/dataMiner.js";
 
 (() => {
     let mailSubmit = document.querySelector('.submit-wrapper');
@@ -31,5 +32,31 @@ import { SendMail } from "./components/mailer.js";
             
     }
 
+    console.log('loaded');
+    
+    function popErrorBox(message) {
+        alert("Something Dun Goofed");
+    }
+
+    function handleDataSet(data) {
+        let userSection = document.querySelector('.fave-section'),
+            userTemplate = document.querySelector('#fave-template').content;
+
+        for (let user in data) {
+            let currentUser = userTemplate.cloneNode(true),
+                currentUserText = currentUser.querySelector('.fave').children;
+
+            currentUserText[1].src = `images/${data[user].avatar}`;
+            currentUserText[2].textContent = data[user].name;
+            currentUserText[3].textContent = data[user].info;
+            currentUserText[4].textContent = data[user].bio;
+
+          
+            userSection.appendChild(currentUser);
+        }
+    }
+
+
     mailSubmit.addEventListener("click", processMail);
+    fetchData("./includes/functions.php").then(data => handleDataSet(data)).catch(err => { console.log(err); popErrorBox(err); });
 })();
